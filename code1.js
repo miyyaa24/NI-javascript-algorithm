0e1f935eeb64558524cc25880ce13935e7be1afc
naim <rlaskdla97@gmail.com>
2023-01-12 21:23:45 +0900

    compareOnlyAlphabet

---
+++ code1.js
@@ -0,0 +1,9 @@
+function compareOnlyAlphabet(str1, str2) {
+    // TODO: 여기에 코드를 작성합니다.
+  
+    resultA = str1.toUpperCase();
+    resultB = str2.toUpperCase();
+  
+    return resultA === resultB;
+  
+  }
