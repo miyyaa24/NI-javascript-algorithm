8f6385bcd3131a5cdfcd718b636f3b696c51d76d
naim <rlaskdla97@gmail.com>
2023-01-18 23:35:48 +0900

    codestates_03_makeDigits2

---
+++ 03_makeDigits2.js
@@ -0,0 +1,10 @@
+function makeDigits2(num) {
+    let result = 1;
+    let count = 2;
+  
+    while (count <= num) {
+      result = result + `-${count}`;
+      count++;
+    }
+    return String(result);
+  }  
