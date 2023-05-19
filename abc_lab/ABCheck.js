f2a820123b590f36219d75666d0544a37a7fb91c
NAIM KIM <94007890+miyyaa24@users.noreply.github.com>
2023-05-19 22:47:10 +0900

    0519 ABCheck

---
+++ abc_lab/ABCheck.js
@@ -0,0 +1,23 @@
+// 문자열을 입력받아 문자열 내에 아래 중 하나가 존재하는지 여부를 리턴해야 합니다.
+
+// 'a'로 시작해서 'b'로 끝나는 길이 5의 문자열
+// 'b'로 시작해서 'a'로 끝나는 길이 5의 문자열
+
+function ABCheck(str) {
+    if (str === undefined) {
+      return false;
+    }
+  
+    str = str.toLowerCase();
+  
+    for (let i = 4; i < str.length; i++) {
+      if (
+        (str[i] === 'a' && str[i - 4] === 'b') ||
+        (str[i] === 'b' && str[i - 4] === 'a')
+      ) {
+        return true;
+      }
+    }
+  
+    return false;
+  }
